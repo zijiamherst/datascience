a7eb2caae0b0d0e15e2e23b39bf58fdbcf950020
Ziji Zhou <zijzhou25@amherst.edu>
2021-09-24 21:27:24 -0400

    app with checkbox (not working)

--- labs/lab07_shiny/app.R
+++ labs/lab07_shiny/app.R
@@ -9,6 +9,30 @@ library(stringr)
 library(fivethirtyeight)
 
 #import data
+mad_men_lead <- mad_men %>%
+  mutate(performer = iconv(mad_men$performer,"UTF-8", "UTF-8", sub = "")) %>% 
+  select(performer, lead_notes) %>%
+  mutate(lead_notes = strsplit(x = lead_notes, split = "; ")) %>%
+  unnest(lead_notes) %>%
+  mutate(lead_notes = strsplit(x = lead_notes, split = ", ")) %>%
+  unnest_wider(lead_notes)  %>%
+  rename(film = 2, year = 3) %>%
+  mutate(year = as.integer(year)) %>%
+  select(performer, film, year)  %>%  
+  arrange(performer)
+
+mad_men_support <- mad_men %>%
+  mutate(performer = iconv(mad_men$performer,"UTF-8", "UTF-8", sub = "")) %>% 
+  select(performer, support_notes) %>%
+  mutate(support_notes = strsplit(x = support_notes, split = "; ")) %>%
+  unnest(support_notes) %>%
+  mutate(support_notes = strsplit(x = support_notes, split = ", ")) %>%
+  unnest_wider(support_notes)  %>%
+  rename(film = 2, year = 3) %>%
+  mutate(year = as.integer(year)) %>%
+  select(performer, film, year) %>%  
+  arrange(performer)
+
 mad_men_tidy <- mad_men %>% 
   mutate(performer = iconv(mad_men$performer,"UTF-8", "UTF-8", sub = "")) %>% 
   select(performer, show, show_start, charend, score) %>%
@@ -18,19 +42,31 @@ mad_men_tidy <- mad_men %>%
 #set up input
 name_choices <- unique(mad_men_tidy$performer)
 
+options_choice_names <- c("Supporting role in movies","Leading role in movies","Show roles with appearance in at least half the episodes")
+options_choice_values <- c("lead_notes", "support_notes", "show")
+names(options_choice_values) <- options_choice_names
+
 ############
 #    ui    #
 ############
 ui <- fluidPage(
-  title = "Mad Men Performers After the Show",
+  
+  titlePanel("Mad Men Performers After the Show"),
   
   sidebarLayout(
-    selectizeInput(inputId = "id_name",
-                   label = "Identify performer(s) to be shown:",
-                   choices = name_choices,
-                   selected = NULL,
-                   multiple = TRUE)
-  ,
+    
+    sidebarPanel(
+      selectizeInput(inputId = "id_name",
+                     label = "Identify performer(s) to be shown:",
+                     choices = name_choices,
+                     selected = NULL,
+                     multiple = TRUE),
+      
+      checkboxGroupInput(inputId = "options_name",
+                   label = "Type of Appearance", 
+                   choices = options_choice_names, 
+                   selected = NULL)
+      ),
   
   mainPanel(plotOutput("timeline"))
   
@@ -47,12 +83,29 @@ server <- function(input, output){
       filter(performer %in% input$id_name)
   })
   
-  output$timeline <- renderPlot({
-    name_data() %>%
-      ggplot(aes(y = performer,x = year)) +
-      geom_point() +
-      geom_line(aes(color = show, size = score))
+  lead_data <- reactive({
+    data <- mad_men_lead %>%
+      filter(performer %in% input$id_name)
   })
+  support_data <- reactive({
+    data <- mad_men_support %>%
+      filter(performer %in% input$id_name)
+  })
+  
+  plot_show <- reactive({
+    show %in% input$options_name
+  })
+  
+
+    output$timeline <- renderPlot({
+      name_data() %>%
+        ggplot() +
+        geom_point(aes(y = performer,x = year)) +
+        geom_line(aes(size = score, color = show,y = performer,x = year)) +
+        geom_point(data = lead_data(),aes(y = performer,x = year, color = film)) + 
+        geom_point(data = support_data(),aes(y = performer,x = year, color = film))
+    })
+  
   
 }
 
